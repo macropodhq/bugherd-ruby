331042c5dab352f4551377ed906bc81168c826c2
Vincent Brendel <vincent@VB.local>
2012-12-12 16:34:38 +1100

    use ENV BUGHERD_PROJECT_KEY if present

--- lib/bugherd/helpers/bugherd_helper.rb
+++ lib/bugherd/helpers/bugherd_helper.rb
@@ -1,12 +1,13 @@
 module BugHerdHelper
 
   def bugherd_tag(options = {})
+    key = ENV['BUGHERD_PROJECT_KEY'] || BugHerd.configuration.project_key
     javascript_tag "
       var BugHerdConfig = #{options.to_json};
       (function (d, t) {
         var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
         bh.type = 'text/javascript';
-        bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{BugHerd.configuration.project_key}';
+        bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{key}';
         s.parentNode.insertBefore(bh, s);
       })(document, 'script');
     "
