a1686f5713f95e1b910807cf3aa2d9dabbad1045
Vincent Brendel <vincent@bugherd.com>
2013-11-27 01:36:37 +0100

    Add turbolinks compat

--- lib/bugherd/helpers/bugherd_helper.rb
+++ lib/bugherd/helpers/bugherd_helper.rb
@@ -2,15 +2,32 @@ module BugHerdHelper
 
   def bugherd_tag(options = {})
     key = ENV['BUGHERD_PROJECT_KEY'] || BugHerd.configuration.project_key
-    javascript_tag "
-      var BugHerdConfig = #{options.to_json};
-      (function (d, t) {
-        var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
-        bh.type = 'text/javascript';
-        bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{key}';
-        s.parentNode.insertBefore(bh, s);
-      })(document, 'script');
-    "
+    if options[:turbolinks]
+      javascript_tag "
+        var BugHerdConfig = #{options.to_json};
+        (function (d, t) {
+          var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+          bh.type = 'text/javascript';
+          bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{key}';
+          s.parentNode.insertBefore(bh, s);
+        })(document, 'script');
+      "
+    else
+      javascript_tag "
+        var BugHerdConfig = #{options.to_json};
+        $(function() {
+          if (window._bugHerd) {
+            window._bugHerd.loadingComplete = false;
+          }
+          (function (d, t) {
+            var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+            bh.type = 'text/javascript';
+            bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{key}';
+            s.parentNode.insertBefore(bh, s);
+          })(document, 'script');
+        });
+      "
+    end
   end
 
 end
