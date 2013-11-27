27722b8cf7d254130178a42607be023200439d4e
Vincent Brendel <vincent@bugherd.com>
2013-11-27 01:52:22 +0100

    separate helper for turbolinks

--- lib/bugherd/helpers/bugherd_helper.rb
+++ lib/bugherd/helpers/bugherd_helper.rb
@@ -2,32 +2,33 @@ module BugHerdHelper
 
   def bugherd_tag(options = {})
     key = ENV['BUGHERD_PROJECT_KEY'] || BugHerd.configuration.project_key
-    if options['turbolinks']
-      javascript_tag "
-        var BugHerdConfig = #{options.to_json};
-        $(function() {
-          if (window._bugHerd) {
-            window._bugHerd.loadingComplete = false;
-          }
-          (function (d, t) {
-            var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
-            bh.type = 'text/javascript';
-            bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{key}';
-            s.parentNode.insertBefore(bh, s);
-          })(document, 'script');
-        });
-      "
-    else
-      javascript_tag "
-        var BugHerdConfig = #{options.to_json};
+    javascript_tag "
+      var BugHerdConfig = #{options.to_json};
+      (function (d, t) {
+        var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        bh.type = 'text/javascript';
+        bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{key}';
+        s.parentNode.insertBefore(bh, s);
+      })(document, 'script');
+    "
+  end
+
+  def bugherd_turbolink_tag(options = {})
+    key = ENV['BUGHERD_PROJECT_KEY'] || BugHerd.configuration.project_key
+    javascript_tag "
+    var BugHerdConfig = #{options.to_json};
+      $(function() {
+        if (window._bugHerd) {
+          window._bugHerd.loadingComplete = false;
+        }
         (function (d, t) {
           var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
           bh.type = 'text/javascript';
           bh.src = '//www.bugherd.com/sidebarv2.js?apikey=#{key}';
           s.parentNode.insertBefore(bh, s);
         })(document, 'script');
-      "
-    end
+      });
+    "
   end
 
 end
